c53f9e92e9eb43768c9f0a7b64ed3bca1a16dded
sgtXn <alascorchy@gmail.com>
2020-08-11 22:26:22 +0300

    Machine-independent rewrite

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
 
+HOSTNAME=$(cat /etc/hostname)
+PCHOST='sgt'
+LAPTOPHOST='sgtxn-laptop'
+
 # Terminate already running bar instances
 killall -q polybar
 
 # Wait until the processes have been shut down
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
-# Launch bar 
-polybar sgtxn -r 
+if [ $HOSTNAME = $PCHOST ]; then
+    polybar pc -r
+fi
 
-echo "Polybar launched"
+if [ $HOSTNAME = $LAPTOPHOST ]; then
+    polybar laptop -r
+fi
