0eb018c958db4878b3f51eecffd1b2bbe9cfa39b
Dmitry Andreev <dmitry.andreev@relexsolutions.com>
2023-05-15 09:23:37 +0300

    lot of changes + qtile is back

--- .config/awesome/autostart.lua
+++ .config/awesome/autostart.lua
@@ -1,7 +1,7 @@
 local awful = require "awful"
 local gfs = require "gears.filesystem"
 
-local sh_path = gfs.get_configuration_dir() .. "autostart.sh"
+local sh_path = gfs.get_xdg_config_home() .. "autostart.sh"
 
 local function load_autostart()
    awful.spawn("bash " .. sh_path)
